11d920f1bc9aae2e0ed70b215a3140becfc2b002
soranuel <soranuel@gmail.com>
2013-11-30 23:44:26 +0700

    Project_php

---
+++ Project_php/index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>SiPRATAMA</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="superfish/src/css/superfish.css" media="screen">
+<script src="superfish/src/js/jquery.js"></script>
+<script src="superfish/src/js/hoverIntent.js"></script>
+<script src="superfish/src/js/superfish.js"></script>
+<script>
+
+
+
+    $(document).ready(function () {
+
+        // initialise plugin
+        $('.nav').superfish();
+        //add options here if required
+
+    });
+
+
+
+
+		</script>
+</head>
+
+<body>
+<div id="body_wrapper">
+	<div id="wrapper">
+		<div id="header">
+            <div id="site_title"><h1><a href="" rel="nofollow">-</a></h1></div>
+            <div id="menu">
+            <ul class="sf-menu nav" id="example">
+				<li class="current"><a href="index.php">Beranda</a></li>
+				<li>
+					<a href="#">Menu</a>
+					<ul>
+                		<li>
+							<a href="menukecelakaan.php">Pertolongan Pertama kecelakaan</a>
+						</li>
+                    	<li>
+							<a href="followed.html">Pertolongan Pertama Bencana</a>
+						</li>
+						<li>
+                        	<a href="followed.html">Tips dan Saran</a>
+                        </li>
+					</ul>
+				</li>
+				<li><a href="download.html">Unduh</a></li>
+            	<li><a href="pelacakan.html">Pelacakan</a></li>
+                <li><a href="Komentar.php" class="last">About Us</a>
+                	<ul>
+                        <li><a href="#">Biodata</a></li>
+                        <li><a href="#">The Team</a></li>
+                    </ul>
+                </li>
+			</ul>
+            </div> <!-- end of menu -->
+        </div><!-- end of header -->
+        
+        <div id="middle">
+          <div id="mid_left">
+                <div id="mid_title">Pertolongan Pertama</div>
+                <p>Tidak ada seorang pun yang dapat memperkirakan kapan kecelakaan atau serangan sakit mendadak akan terjadi.</p>
+                <a rel="nofollow" href="" class="viewall"></a>
+            </div>
+        <img src="images/first.png" />
+        </div> <!-- tutup middle -->
+
+    </div><!-- tutup wrapper -->
+</div><!-- tutup body wrapper -->
+
+	<div class="rata" align="justify">
+	<div id="sipratama_main">
+    	<div class="cbox_fw">
+        	<div class="cbox_3b fp_box"><span class="b1"></span>
+            	<h4><a href="www.facebook.com">FACEBOOK</a></h4>
+                <p>GO AND LIKE US IN FACEBOOK</p>
+            </div>
+        	<div class="cbox_3b fp_box"><span class="b2"></span>
+            	<h4><a href="www.twitter.com">TWITTER</a></h4>
+                <p>FOLLOW US IN TWITTER </p>
+            </div>
+        	<div class="cbox_3b fp_box"><span class="b3"></span>
+            	<h4><a href="www.google.com">GOOGLE+</a></h4>
+                <p>FOLLOW US IN GOOGLE+</p>
+            </div>
+            <div class="cleaner"></div>
+        </div>
+        <div class="cbox_fw">
+        	<div class="cbox_large float_l">
+            	<h2>SELAMAT DATANG</h2>
+                <p>Kami menyediakan berbagai cara untuk melakukan pertolongan pertama baik dalam kecelakaan ataupun serangan sakit yang mendadak terjadi,dan juga kami menyediakan fitur pelacakan darurat agar mudah menemukan lokasi kecelakaan karena kecelakaan bisa menimpa siapa saja dan kapan saja.Kami berharap website kami dapat berguna bagi anda semua para pembaca</p>
+                <div class="cleaner h20"></div>	
+                <div class="cbox_small float_l">
+                	<h5 class="tw_bullet">Pertolongan Pertama Luka Gores</h5>
+                    <img src="images/lukagores.jpg" class="image_frame" />
+                    <p>Pernahkah anda mengalami luka karena teriris pisau? Atau luka karena tersayat benda tajam lainnya? Saya yakin tentu sebeagian besar orang pernah mengalaminya. Dan barangkali, tidak hanya satu atau dua kali terjadi dalam hidupnya. Lalu bagaimanakah anda merawat luka tersebut? Membiarkannya sembuh sendiri kah? Atau langsung membalutnya dengan plester luka? Benarkah anda yakin cara tersebut yang terbaik dalam merawat dan mengobati luka iris di kulit anda? Lantas bagaimanakah sebenarnya cara merawat dan mengobati luka iris yang benar?</p>
+                    <a href="luka_iris.php"><span>&gt;&gt;</span> Read More</a>
+                 </div>
+                 <div class="cbox_small float_r">
+                 	<h5 class="tw_bullet">Mendadak Tersedak</h5>
+                    <img src="images/tersedak.png" class="image_frame" />
+                    <p>Pernahkah anda tersedak ketika sedang makan? Ya, saya yakin setiap orang pernah merasakannya. Apa yang anda kemudian lakukan untuk meredakannya? Minum? Ya beberapa jawaban pasti menyebutkan minum. Tapi bagaimana sebetulnya yang harus kita lakukan ketika tersedak. Dan apa yang harus kita lakukan ketika semisal teman semeja kita yang tersedak?
+
+Tersedak terjadi ketika benda asing bersarang di pintu masuk tenggorokan, menutupi jalan nafas kita.  Pada dewasa, makanan menjadi penyebab tersering. Sedangkan pada anak anak biasanya benda – benda kecil yang potensial.</p>
+					<a href="tersedak.php"><span>&gt;&gt;</span> Read More</a>
+                 </div>
+                  <div class="cleaner h20"></div>	
+                <div class="cbox_small float_l">
+                	<h5 class="tw_bullet">Informasi Tentang Obesitas</h5>
+                    <img src="images/Obesity.jpg" class="image_frame" />
+                    <p>Obesitas merupakan suatu kondisi dimana kelebihan jaringan adipose pada tubuh manusia. Adipose dalam bahasa sehari-sehari disebut jaringan lemak. Peningkatan akumulasi jaringan lemak ini biasanya tertimbun di dalam tubuh manusia seperti omentum (pelapis dinding dalam perut dan organ dalam), bawah kulit dan jaringan lemak lain yang tersebar diseluruh tubuh manusia. Pengukuran jaringan lemak tubuh dapat dilakukan dengan mengukur BMI (Body Mass Index) yang dapat dihitung berdasarkan rumus sederhana yaitu berat badan (Kilogram) dibagi kuadrat dari tinggi badan dalam meter.</p>
+                    <a href="obesitas.php"><span>&gt;&gt;</span> Read More</a>
+                 </div>
+                 <div class="cbox_small float_r">
+                 	<h5 class="tw_bullet">Mengatasi luka bakar</h5>
+                    <img src="images/lukabakar.jpg" class="image_frame" />
+                    <p>Luka bakar derajat satu. Luka bakar derajat satu hanya mengenai lapisan luar kulit yang menimbulkan peradangan lokal pada daerah tersebut. Sunburn sering dimasukan dalam klasifikasi luka bakar ini. Peradangan yang terjadi ditandai dengan rasa nyeri, kemerahan dan pembengkakan ringan. Kulit terasa sangat perih atau nyeri saat disentuh.
+
+Luka bakar derajat dua. Luka bakar mengenai kulit yang lebih dalam dan menimbulkan rasa sakit, kemerahan dan inflamasi yang lebih parah. Kulit juga tampak melepuh.Luka bakar derajat tiga. Luka bakar yang sangat dalam</p>
+					<a href="lukabakar.php"><span>&gt;&gt;</span> Read More</a>
+                 </div>
+                 <div class="cleaner h20"></div>	
+                <div class="cbox_small float_l">
+                	<h5 class="tw_bullet">Pertolongan Pertama Pada Sakit Jantung</h5>
+                    <img src="images/seranganjantung.jpg" class="image_frame" />
+                    <p>Serangan jantung adalah kondisi yang membutuhkan penanganan yang cepat dan termasuk salah satu kegawatdaruratan medis. Semakin cepat tindakan atau penanganan yang diberikan maka akan semakin besar kemungkinan penderita dapat diselamatkan.
+ 
+Sebelum mengambil tindakan-tindakan pertolongan, yang paling penting adalah kenali gejala dan pastikan apakah keluhan yang dialami merupakan gejala serangan jantung. Gejala serangan jantung dapat bervariasi, dari derajat yang paling ringan hingga berat. Secara umum, gejala serangan jantung adalah sebagai berikut:</p>
+                    <a href="seranganjantung.php"><span>&gt;&gt;</span> Read More</a>
+                 </div>
+                 <div class="cbox_small float_r">
+                 	<h5 class="tw_bullet">Pertolongan Pertama Keracunan Makanan</h5>
+                    <img src="images/keracunanmakanan.jpg" class="image_frame" />
+                    <p>salah satu bahaya kesehatan yang mungkin terjadi adalah keracunan makanan. Korban keracunan makanan harus segera mendapatkan pertolongan medis. Pada jalur pemudik, pemerintah biasanya menyediakan posko-posko kesehatan yang memberikan pelayanan 24 jam untuk menangani masalah kesehatan yang terjadi pada para pemudik.
+ 
+Namun, sebelum penderita keracunan makanan dibawa ke fasilitas kesehatan terdekat, ada beberapa pertolongan pertama yang dapat kita lakukan:</p>
+					<a href="keracunanmakanan.php"><span>&gt;&gt;</span> Read More</a>
+                 </div>
+                  <div class="cleaner h20"></div>	
+                <div class="cbox_small float_l">
+                	<h5 class="tw_bullet">Pertolongan Pertama Pada Penderita Asma</h5>
+                    <img src="images/asma.jpg" class="image_frame" />
+                    <p>Pertolongan Pertama pada Penderita Asma (Sesak Napas). Asma merupakan penyakit yang sangat berbahaya jika dibiarkan terus menerus. Penyakit ini dapat membuat orang menjadi sangat panik, apalagi jika orang tersebut belum berpengalaman tentang penyakit ini. Sebelumnya sudah saya jelaskan secara detail tentang gejala, dan cara mengobati penyakit asma.Disini akan dibahas tentang pertolongan pertama pada penderita asma yang sedang kambuh dan kebetulan tidak membawa obat asma. Saya sendiri pernah mengalami hal ini dan menjadi sangat panik karena melihat asma seseorang yang sedang kambuh dan kesulitan untuk bernapas. Jika anda sudah tidak sabar untuk mengetahuinya, silahkan simak dengan baik. 
+</p>
+                    <a href="asma.php"><span>&gt;&gt;</span> Read More</a>
+                 </div>
+                 <div class="cbox_small float_r">
+                 	<h5 class="tw_bullet">Pertolongan Pertama Pada Kecelakaan</h5>
+                    <img src="images/kecelakaan.jpg" class="image_frame" />
+                    <p>Kecelakaan bisa terjadi dimana saja termasuk saat berolahraga seperti kecelakaan bersepeda, tabrakan saat berlari, dan masih banyak lagi. Bagaimana cara memberi pertolongan pertama? Berikut ini adalah caranya!
+
+Sebelum melakukan pertolongan, ada baiknya Anda mengetahui tahapan-tahapan dalam memberi pertolongan pertama karena sangat penting untuk memberikan kesempatan hidup yang lebih besar bagi korban. Salah tahapan dalam melakukan pertolongan pertama dapat berakibat fatal pada korban, bahkan bisa menjadi "tangan maut" bagi si korban.
+
+Berikut tahapan-tahapan untuk melakukan </p>
+					<a href="kecelakaan.php"><span>&gt;&gt;</span> Read More</a>
+                 </div>
+            </div>
+            <div class="cbox_small float_r">
+            	<h2>Recent Post</h2>
+                	<div class="rp_box">
+                    	<img src="images/baby.png" /><p>Mana yang lebih berat? Diare pada anak-anak atau diare pada bayi?</p>
+                        <a href="#" class="more float_r"><span>&gt;&gt;</span> Read More</a>
+                        <div class="cleaner"></div>
+                    </div>
+                    <div class="rp_box">
+                    	<img src="images/lukagores.png" /><p>Pertolongan Pertama Luka Gores</p>
+                        <a href="#" class="more float_r"><span>&gt;&gt;</span> Read More</a>
+                    	<div class="cleaner"></div>
+                    </div>
+                    <div class="rp_box">
+                    	<img src="images/Obesity.png" /><p>Lebih Jauh tentang Obesitas</p>
+                        <a href="#" class="more float_r"><span>&gt;&gt;</span> Read More</a>
+                        <div class="cleaner"></div>
+                    </div>
+            </div>
+        </div><!-- tutup cbox_fw -->
+    </div><!-- tutup sipratama main -->
+    </div><!-- tutup rata -->
+</body>
+</html>
